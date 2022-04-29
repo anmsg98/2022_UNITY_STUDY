85ad139d420d67ea6dbbeef02475d7915d773fdc
Jeon Seong Hee <wjstjd1004@naver.com>
2022-04-29 12:22:33 +0900

    9주차 완료 (인스턴싱)

---
+++ Assets/Script/ManagedPrefabDatabase.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(order = 0, fileName = "PrefabDatabase", menuName = "Create Prefab DB")]
+
+public class ManagedPrefabDatabase : ScriptableObject
+{
+    public ManagedPrefab[] prefabs;
+}
+
+[Serializable]
+public class ManagedPrefab
+{
+    public string prefabName;
+    public GameObject prefabGameObject;
+}
